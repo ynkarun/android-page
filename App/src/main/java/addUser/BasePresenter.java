4ee64e338045003c3e14ef4666b90413eeb4d01a
ynkarun <98223336+ynkarun@users.noreply.github.com>
2022-01-23 21:29:39 +0530

    Add files via upload

---
+++ App/src/main/java/addUser/BasePresenter.java
@@ -0,0 +1,7 @@
+package com.arun.medicinetime;
+
+
+public interface BasePresenter {
+
+    void start();
+}
---
+++ App/src/main/java/addUser/BaseView.java
@@ -0,0 +1,6 @@
+package com.arun.medicinetime;
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+}
